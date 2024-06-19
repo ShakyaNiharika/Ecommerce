c2d4747b8263d2bf6a7c1376efae9aff28e23a9c
ShakyaNiharika <145098349+ShakyaNiharika@users.noreply.github.com>
2024-06-19 22:44:50 +0545

    added button to shop now

--- src/components/shopNow(second)/ShopNowDis.jsx
+++ src/components/shopNow(second)/ShopNowDis.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { fourthShop } from "../../data/shopNow/ShopNow.js";
 import "../../styles/shopNowDis/ShopNowDis.css";
+import Button from "../button/Button.jsx";
 
 const ShopNowDis = () => {
   return (
-    <div className="mainShopNow-dis">
-      {fourthShop.map((items) => (
-        <div className="outlet" key={items?.id}>
-          <img className="fruitsImages" src={items?.image} />
-          <h1>{items?.head}</h1>
-          <h2>{items?.title}</h2>
-        </div>
-      ))}
+    <div className="mainShopNowDis">
+      {fourthShop.map((items) => {
+        return (
+          <div className="outlet" key={items?.id}>
+            <img className="fruitsImages" src={items?.image} />
+            <h1>{items?.head}</h1>
+            <h2>{items?.title}</h2>
+            <Button className="shopButton" title="Shop Now" />
+          </div>
+        );
+      })}
     </div>
   );
 };
